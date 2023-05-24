e767068e62653beaf0dfc1403cd5270f2f3f06aa
jasonj <jasonj@live.cn>
2023-05-24 13:45:30 +0800

    add u-boot command "netboot" related macros

--- include/configs/dragonli3.h
+++ include/configs/dragonli3.h
@@ -38,23 +38,66 @@
 #define CONFIG_DW_GMAC_DEFAULT_DMA_PBL	(8)
 #define CONFIG_DW_ALTDESCRIPTOR
 
-#define CONFIG_SYS_HZ_CLOCK		1000000	/* Timer is clocked at 1MHz */
+#define CONFIG_SYS_HZ_CLOCK		    1000000	/* Timer is clocked at 1MHz */
 
 #define CONFIG_CMDLINE_TAG
 #define CONFIG_SETUP_MEMORY_TAGS
 #define CONFIG_INITRD_TAG
 #define CONFIG_REVISION_TAG
 
-#define CONFIG_SYS_CBSIZE		1024
+#define CONFIG_SYS_CBSIZE		    	SZ_1K
 
-#define CONFIG_SYS_MALLOC_LEN		(1 * 1024 * 1024)
+#define CONFIG_SYS_MALLOC_LEN			SZ_1M
 
 #define BOOT_TARGET_DEVICES(func) \
 	func(MMC, mmc, 0)
 
+
+#define CONFIG_ETHADDR                  AA:CC:DD:11:22:33
+#define CONFIG_NETMASK                  255.255.255.0
+#define CONFIG_IPADDR                   192.168.10.234
+#define CONFIG_GATEWAYIP                192.168.10.1
+#define SERVERIP                		192.168.10.30
+
+#ifndef MK_STR
+#define STR1(x)     	#x
+#define MK_STR(x)       STR1(x)
+#endif
+
+#define SPI_IMG_OFFSET               	SZ_128K  /* Kernel is stored in SPI Flash (offset at 128KB) */
+#define SPI_FDT_OFFSET                  SZ_8K    /* the first 8KB is used to save env in SPI Flash */
+
+#define CUST_ENV_SETTINGS                                      \
+        "serverip=192.168.10.128\0"                            \
+        "args=setenv bootargs " CONFIG_BOOTARGS "\0"    	   \
+        "image=zImage\0"                                       \
+        "spiaddr_img=" MK_STR(SPI_IMG_OFFSET) "\0" 		       \
+		"spiaddr_fdt=" MK_STR(SPI_FDT_OFFSET) "\0"   		   \
+        "spisize=0\0"	                                       \
+        "spiprobe=sf probe\0"                                  \
+        "addip=setenv bootargs ${bootargs}"                    \
+        " ip=${ipaddr}:${serverip}:${gatewayip}:"              \
+        " ${netmask}:${hostname}:eth0:off\0"                   \
+        "flashboot=run args addip;run spiprobe;"               \
+        " sf read ${kernel_addr_r} ${spiaddr} ${spisize};"     \
+        " bootm ${kernel_addr_r}\0"                            \
+        "netboot="                                             \
+        " tftp ${kernel_addr_r} ${image};"                     \
+        " tftp ${fdt_addr_r} ${fdtfile};"                      \
+        " run args addip;"                                     \
+        " bootz ${kernel_addr_r} - ${fdt_addr_r}\0"            \
+        "update=run spiprobe;" 								   \
+		" tftp ${fdt_addr_r} ${fdtfile};"					   \
+        " sf erase ${spiaddr_fdt} +${filesize};"               \
+        " sf write ${fdt_addr_r} ${spiaddr_fdt} ${filesize};"  \
+		" tftp ${kernel_addr_r} ${image};"					   \
+        " sf erase ${spiaddr_img} +${filesize};"               \
+        " sf write ${kernel_addr_r} ${spiaddr_img} ${filesize}\0"
+
 #include <config_distro_bootcmd.h>
 #define CONFIG_EXTRA_ENV_SETTINGS				\
-			"kernel_addr_r=0xC0008000\0"		\
+			CUST_ENV_SETTINGS					\
+			"kernel_addr_r=0xC0000000\0"		\
 			"fdtfile=dragonli3.dtb\0"	        \
 			"fdt_addr_r=0xC0408000\0"		    \
 			"scriptaddr=0xC0418000\0"		    \
